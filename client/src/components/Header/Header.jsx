e213f182cd91d66beb543de033a4acdb5806588d
ViktorsLV <viktors.ch11@gmail.com>
2022-04-29 18:45:12 +0200

    created yaml file

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header({title}) {
+    return (
+        <>
+            <h1 style={{margin: '0 auto', fontSize: 36}}>{title}</h1>
+        </>
+    )
+}
