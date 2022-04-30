1b16024d2af40c83a9db9d87dd158d27f2b3393f
ViktorsLV <viktors.ch11@gmail.com>
2022-04-30 22:07:01 +0200

    added Tailwind to project

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-      <Header title="Hello World!"/>
+      <Header title="Hello World!" />
     </div>
   );
 }
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header({title}) {
     return (
         <>
-            <h1 style={{margin: '0 auto', fontSize: 36}}>{title}</h1>
+            <h1 className='text-red-500' style={{margin: '0 auto', fontSize: 36}}>{title}</h1>
         </>
     )
 }
