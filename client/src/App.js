fd99a496963fb1538487b9e69caf809b2fc4a771
ViktorsLV <viktors.ch11@gmail.com>
2022-05-07 17:19:10 +0200

    User authentication implemented with auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import Navigation from './components/Navigation';
+
+// Pages
 import About from './pages/About';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <div className=''>
-      <Navigation />
+        <Navigation />
 
-      {/* main content */}
-      <div className='p-5 max-w-7xl mx-auto'>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="blog" element={<Blog />} />
-        </Routes>
-      </div>
+        {/* main content */}
+        <div className='p-5 max-w-7xl mx-auto'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="login" element={<Login />} />
+            <Route path="profile" element={<Profile />} />
+          </Routes>
+        </div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN}
+        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+        redirectUri={window.location.origin}
+      >
+        <App />
+      </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
