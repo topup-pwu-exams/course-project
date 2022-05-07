ac5dd13b82d17c736b30fd61c9603643a0b33952
ViktorsLV <viktors.ch11@gmail.com>
2022-05-07 17:46:25 +0200

    Styling of nav

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link, NavLink } from "react-router-dom";
 import logo from '../assets/Images/logo_darker.png';
 import { useAuth0 } from "@auth0/auth0-react";
+import { LoginIcon, LogoutIcon } from '@heroicons/react/outline'
+
 
 const Navigation = () => {
     const { user, isAuthenticated, isLoading } = useAuth0();
@@ -37,19 +39,24 @@ const Navigation = () => {
             </div>
 
             <div className='flex items-center space-x-8 border-l-2 border-gray-300 m-2 text-gray-500'>
-                
+
                 {!isAuthenticated && (
-                    <h3 onClick={() => loginWithRedirect()} className='ml-8 hover:border-purple-500 hover:rounded-full border-2 border-transparent px-3 py-1 hover:cursor-pointer'>Log in</h3>
+                    <div onClick={() => loginWithRedirect()} className='ml-8 flex flex-row hover:border-purple-500 rounded-full border-2 border-transparent bg-purple-500 text-white hover:text-purple-500 hover:bg-white px-3 py-1 hover:cursor-pointer'>
+                        <h3 >Log in </h3> <LoginIcon className='h-5 w-auto ml-2 mt-0.5' />
+                    </div>
                 )}
                 {isAuthenticated && (
                     <>
-                        <NavLink to='/profile' className={({ isActive }) => isActive ? profileActive : 'rounded-full border-2'} style={{marginLeft: '2rem'}}>
+                        <NavLink to='/profile' className={({ isActive }) => isActive ? profileActive : 'rounded-full border-2'} style={{ marginLeft: '2rem' }}>
                             {/* <h3 className='rounded-full border-2 border-purple-500 px-3 py-1 hover:text-white hover:bg-purple-500 hover:border-transparent hover:cursor-pointer'>Profile</h3> */}
                             {/* <div className='rounded-full border-2 hover:border-purple-500'> */}
-                                <img src={user.picture} alt={user.name} className='h-9 w-auto object-fill rounded-full'/>
+                            <img src={user.picture} alt={user.name} className='h-9 w-auto object-fill rounded-full' />
                             {/* </div> */}
                         </NavLink>
-                        <h3 onClick={() => logout({ returnTo: window.location.origin })} className='ml-8 hover:border-purple-500 hover:rounded-full border-2 border-transparent px-3 py-1 hover:cursor-pointer'>Log out</h3>
+                        {/* <h3 onClick={() => logout({ returnTo: window.location.origin })} className='ml-8 hover:border-purple-500 hover:rounded-full border-2 border-transparent px-3 py-1 hover:cursor-pointer'>Log out</h3> */}
+                        <div onClick={() => logout({ returnTo: window.location.origin })} className='ml-8 flex flex-row hover:border-transparent rounded-full border-2 border-purple-500 bg-white text-purple-500 hover:text-white hover:bg-purple-500 px-3 py-1 hover:cursor-pointer'>
+                            <h3 >Log out </h3> <LogoutIcon className='h-5 w-auto ml-2 mt-0.5' />
+                        </div>
                     </>
                 )}
             </div>
