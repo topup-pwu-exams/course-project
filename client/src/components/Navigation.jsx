36cdc4d8d1777fc7f0af564068eed98187ffb2b3
ViktorsLV <viktors.ch11@gmail.com>
2022-05-11 22:48:40 +0200

    setting user in cookie, restricting routes, store

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import { Link, NavLink } from "react-router-dom";
+import React, { useContext } from 'react';
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import logo from '../assets/Images/logo_darker.png';
 import { LoginIcon, LogoutIcon } from '@heroicons/react/outline'
-
+import jsCookie from 'js-cookie'
+import { Store } from '../utils/Store';
 
 const Navigation = () => {
+    const navigate = useNavigate();
+    const { state, dispatch } = useContext(Store);
+
+    const { userInfo } = state;
+
     const pages = [
         // { name: 'Explore', url: 'explore', id: 1 },
         { name: 'About', url: 'about', id: 2 },
@@ -14,6 +20,13 @@ const Navigation = () => {
 
     const activeLink = 'text-purple-500'
     const profileActive = 'border-purple-500 rounded-full border-2'
+
+    const logoutClickHandler = () => {
+        dispatch({ type: 'USER_LOGOUT' });
+        jsCookie.remove('userInfo');
+        jsCookie.remove('cartItems');
+        navigate('/');
+    };
 
     return (
         <header className='flex justify-between p-5 max-w-7xl mx-auto'>
@@ -35,19 +48,22 @@ const Navigation = () => {
 
             <div className='flex items-center space-x-8 border-l-2 border-gray-300 m-2 text-gray-500'>
 
-                <NavLink to='/login'>
-                    <div className='ml-8 flex flex-row hover:border-purple-500 rounded-full border-2 border-transparent bg-purple-500 text-white hover:text-purple-500 hover:bg-white px-3 py-1 hover:cursor-pointer'>
-                        <h3 >Log in </h3> <LoginIcon className='h-5 w-auto ml-2 mt-0.5' />
-                    </div>
-                </NavLink>
+                {!userInfo ?
+                    <NavLink to='/login'>
+                        <div className='ml-8 flex flex-row hover:border-purple-500 rounded-full border-2 border-transparent bg-purple-500 text-white hover:text-purple-500 hover:bg-white px-3 py-1 hover:cursor-pointer'>
+                            <h3 >Log in </h3> <LoginIcon className='h-5 w-auto ml-2 mt-0.5' />
+                        </div>
+                    </NavLink> :
+                    <>
+                        <NavLink to='/profile' className={({ isActive }) => isActive ? profileActive : 'rounded-full border-2 hover:border-purple-500'} style={{ marginLeft: '2rem' }}>
+                            <img src={userInfo.picture} alt={userInfo.given_name} className='h-9 w-auto object-fill rounded-full' />
+                        </NavLink>
 
-                {/* <NavLink to='/profile' className={({ isActive }) => isActive ? profileActive : 'rounded-full border-2'} style={{ marginLeft: '2rem' }}>
-                    <img src={user.picture} alt={user.name} className='h-9 w-auto object-fill rounded-full' />
-                </NavLink> */}
-
-                <div className='ml-8 flex flex-row hover:border-transparent rounded-full border-2 border-purple-500 bg-white text-purple-500 hover:text-white hover:bg-purple-500 px-3 py-1 hover:cursor-pointer'>
-                    <h3 >Log out </h3> <LogoutIcon className='h-5 w-auto ml-2 mt-0.5' />
-                </div>
+                        <div onClick={logoutClickHandler} className='ml-8 flex flex-row hover:border-transparent rounded-full border-2 border-purple-500 bg-white text-purple-500 hover:text-white hover:bg-purple-500 px-3 py-1 hover:cursor-pointer'>
+                            <h3 >Log out </h3> <LogoutIcon className='h-5 w-auto ml-2 mt-0.5' />
+                        </div>
+                    </>
+                }
             </div>
         </header>
     )
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { StoreProvider } from "./utils/Store";
 
 ReactDOM.render(
   <Router>
-    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
-      <App />
-    </GoogleOAuthProvider>
+    <StoreProvider>
+      <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
+        <App />
+      </GoogleOAuthProvider>
+    </StoreProvider>
   </Router>,
   document.getElementById('root'),
 );
