5132b6707c68c6687f9ba6558460f8329785db8e
ViktorsLV <viktors.ch11@gmail.com>
2022-05-12 00:10:01 +0200

    BaseButton import fix in testing

--- client/src/components/common/BaseButton/__test__/BaseButton.test.js
+++ client/src/components/common/BaseButton/__test__/BaseButton.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import BaseButton from '../../../BaseButton/BaseButton';
+import BaseButton from '../BaseButton';
 
 describe("BaseButton", () => {
     it('should render same text passed into text prop', () => {
