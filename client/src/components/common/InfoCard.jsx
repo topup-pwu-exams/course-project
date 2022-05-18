bc81bbf80fbd2abaa1464ea4c865e255ac68d4ab
kizza <antonio.jera10@gmail.com>
2022-05-18 15:32:54 +0100

    cards styled

--- client/src/components/common/InfoCard.jsx
+++ client/src/components/common/InfoCard.jsx
@@ -3,11 +3,13 @@ import { DesktopComputerIcon } from '@heroicons/react/outline'
 
 function InfoCard() {
   return (
-    <div className='border-2 border-white shadow-md w-1/4 rounded-2xl'>
+    <div className='border-2 border-white hover:border-p-color shadow-md w-1/4 rounded-2xl hover:text-white hover:bg-pshade-color'>
         <div className='p-5'>
           <div className='flex flex-col justify-center items-center text-center my-10 mx-5'>
-            <DesktopComputerIcon className='w-14'/>
-            <p className='text-md font-bold mt-5'>14 online courses</p>
+            <div className='w-16 h-16 rounded-full hover:bg-white flex items-center justify-center'>
+              <DesktopComputerIcon className='w-10 h-10 p-color mx-auto overflow-hidden '/>
+            </div>
+            <p className='text-md font-bold mt-5 '>14 online courses</p>
             <p className=''>Explore a variety of fresh courses</p>
           </div>
         </div>
