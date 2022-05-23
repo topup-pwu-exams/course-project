14aa5f63f32b4e0f914eb999a585fa9115ce9782
ViktorsLV <viktors.ch11@gmail.com>
2022-05-23 20:50:25 +0200

    order schema in sanity

---
+++ server/schemas/shippingDetails.js
@@ -0,0 +1,32 @@
+export default {
+    title: 'Shipping Details',
+    name: 'shippingDetails',
+    type: 'object',
+    fields: [
+        {
+            title: 'fullName',
+            name: 'fullName',
+            type: 'string',
+        },
+        {
+            title: 'details',
+            name: 'details',
+            type: 'string',
+        },
+        {
+            title: 'city',
+            name: 'city',
+            type: 'string',
+        },
+        {
+            title: 'postalCode',
+            name: 'postalCode',
+            type: 'string',
+        },
+        {
+            title: 'country',
+            name: 'country',
+            type: 'string',
+        },
+    ],
+};
