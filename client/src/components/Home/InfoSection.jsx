c75279cc926fb6a24cfce08ab6b9456718b64527
ViktorsLV <viktors.ch11@gmail.com>
2022-05-24 16:19:47 +0200

    info section card styling with dynamic icons

--- client/src/components/Home/InfoSection.jsx
+++ client/src/components/Home/InfoSection.jsx
@@ -6,9 +6,9 @@ function InfoSection() {
     <div className='custom-layout mb-10'>
       <h2 className=''>Our Learning Opportunity</h2>
       <div className='flex justify-center items-center space-x-10 mt-5'>
-        <InfoCard text='Online courses'/>
-        <InfoCard text='Online courses'/>
-        <InfoCard text='Online class' icon=''/>
+        <InfoCard text='Explore' description="Explore a variety of different courses" icon="SearchIcon"/>
+        <InfoCard text='Choose' description="Choose courses suitable for you and purchase them" icon="CursorClickIcon"/>
+        <InfoCard text='Learn' description="Schedule a time to learn and get started!" icon="CalendarIcon"/>
       </div>
     </div>
   )
