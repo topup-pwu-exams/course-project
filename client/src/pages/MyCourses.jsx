a0872c5f4bbc0bb9076988ad58633256b8a69baa
ViktorsLV <viktors.ch11@gmail.com>
2022-05-24 14:51:12 +0200

    404 styling

--- client/src/pages/MyCourses.jsx
+++ client/src/pages/MyCourses.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 
 const MyCourses = () => {
-    const tabActive = 'text-purple-500 font-bold border-b-4 border-purple-500'
+    const tabActive = 'text-accent-500 font-bold border-b-4 border-accent-500'
 
     return (
-        <div className='bg-gray-200 custom-layout'>
+        <div className='custom-layout'>
             <h1>My Courses</h1>
             <div className='flex space-x-10 my-10'>
-                <NavLink to='owned' className={({ isActive }) => isActive ? tabActive : 'hover:text-purple-500'}>
+                <NavLink to='owned' className={({ isActive }) => isActive ? tabActive : 'hover:text-accent-500'}>
                     <p className=''>Owned courses</p>
                 </NavLink>
 
-                <NavLink to='liked' className={({ isActive }) => isActive ? tabActive : 'hover:text-purple-500'}>
+                <NavLink to='liked' className={({ isActive }) => isActive ? tabActive : 'hover:text-accent-500'}>
                     <p className=''>Liked courses</p>
                 </NavLink>
             </div>
