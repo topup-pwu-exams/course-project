86ee1c4e8743159fe54195c49edb1580d3559821
ViktorsLV <viktors.ch11@gmail.com>
2022-05-26 22:34:41 +0200

    client comment test

--- client/src/utils/client.js
+++ client/src/utils/client.js
@@ -3,6 +3,7 @@ import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  // projectId: '',
   dataset: 'production',
   apiVersion: '2021-03-25', // use current UTC date - see "specifying API version"!
   token: process.env.REACT_APP_SANITY_TOKEN, // or leave blank for unauthenticated usage
